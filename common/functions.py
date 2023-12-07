d1b972b39028fea4616a0ce305237f4621761674
ParkSangJin <ticani0610@gmail.com>
2023-12-07 18:08:32 +0900

    common file adding doc string

--- common/functions.py
+++ common/functions.py
@@ -2,51 +2,123 @@ import numpy as np
 
 
 
-def identity_function(x):
+def identity_function(x: np.ndarray) -> np.ndarray:
+    """Return the input array as is.
+
+    Args:
+        x (np.ndarray): The input array._
+
+    Returns:
+        np.ndarray: The input array itself.
+    """
     return x
 
 
-def step_function(x):
-    return np.array(x > 0, dtype=np.int32)
+def step_function(x: np.ndarray) -> np.ndarray:
+    """Return the step function output for the input array.
+
+    Args:
+        x (np.ndarray): The input array.
+
+    Returns:
+        np.ndarray: The step function output (0 or 1) based on the input array's elements.
+    """
+    return (x > 0).astype(np.int32)
 
 
-def sigmoid(x):
+def sigmoid(x: np.ndarray) -> np.ndarray:
+    """Compute the sigmoid function for the input array.
+
+    Args:
+        x (np.ndarray): The input array.
+
+    Returns:
+        np.ndarray: The sigmoid function output for each element of the input array.
+    """
     return 1 / (1 + np.exp(-x))
 
 
-def sigmoid_grad(x):
-    return (1.0 - sigmoid(x)) * sigmoid(x)
+def sigmoid_grad(x: np.ndarray) -> np.ndarray:
+    """Compute the gradient of the sigmoid function.
+
+    Args:
+        x (np.ndarray): The input array.
+
+    Returns:
+        np.ndarray: The gradient of the sigmoid function for each element of the input array.
+    """
+    sig = sigmoid(x)
+    return (1.0 - sig) * sig
 
 
-def relu(x):
+def relu(x: np.ndarray) -> np.ndarray:
+    """Compute the Rectified Linear Unit (ReLU) for the input array.
+
+    Args:
+        x (np.ndarray): The input array.
+
+    Returns:
+        np.ndarray: The ReLU output for each element of the input array.
+    """
     return np.maximum(0, x)
 
 
-def relu_grad(x):
-    grad = np.zeros(x)
-    grad[x>=0] = 1
-    return grad
+def relu_grad(x: np.ndarray) -> np.ndarray:
+    """Compute the gradient of the Rectified Linear Unit (ReLU) function.
+
+    Args:
+        x (np.ndarray): The input array.
+
+    Returns:
+        np.ndarray: The gradient of the ReLU function for each element of the input array.
+    """
+    return np.where(x >= 0, 1, 0)
 
 
-def softmax(x):
+def softmax(x: np.ndarray) -> np.ndarray:
+    """Compute the softmax function for the input array.
+
+    Args:
+        x (np.ndarray): The input array.
+
+    Returns:
+        np.ndarray: The softmax output for each element of the input array.
+    """
     if x.ndim == 2:
-        x = x.T
-        x = x - np.max(x, axis=0)
-        y = np.exp(x) / np.sum(np.exp(x), axis=0)
-        return y.T
+        x = x - np.max(x, axis=1, keepdims=True)
+        y = np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+        return y
     
     x = x - np.max(x)
     return np.exp(x) / np.sum(np.exp(x))
 
 
-def mean_squared_error(y, t):
-    return 0.5 * np.sum((y-t)**2)
+def mean_squared_error(y: np.ndarray, t: np.ndarray) -> np.ndarray:
+    """Compute the mean squared error between predicted and target arrays.
+
+    Args:
+        y (np.ndarray): The predicted array.
+        t (np.ndarray): The target array.
+
+    Returns:
+        np.ndarray: The mean squared error between y and t.
+    """
+    return 0.5 * np.sum((y - t)**2)
 
 
-def cross_entropy_error(y, t):
+def cross_entropy_error(y: np.ndarray, t: np.ndarray) -> np.ndarray:
+    """Compute the cross-entropy error between predicted and target arrays.
+
+    Args:
+        y (np.ndarray): The predicted array.
+        t (np.ndarray): The target array.
+
+    Returns:
+        np.ndarray: The cross-entropy error between y and t.
+    """
     if y.ndim == 1:
-        t = t.reshape(1, t.size)
-        y = y.reshape(1, y.size)
+        t = t.reshape(1, -1)
+        y = y.reshape(1, -1)
 
     if t.size == y.size:
         t = t.argmax(axis=1)
@@ -55,6 +127,14 @@ def cross_entropy_error(y, t):
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
 
-def softmax_loss(X, t):
-    y = softmax(X)
-    return cross_entropy_error(y, t)
+def softmax_loss(X: np.ndarray, t: np.ndarray) -> np.ndarray:
+    """Compute the softmax loss between predicted and target arrays.
+
+    Args:
+        X (np.ndarray): The input array.
+        t (np.ndarray): The target array.
+
+    Returns:
+        np.ndarray: The softmax loss between X and t.
+    """
+    return cross_entropy_error(softmax(X), t)
--- common/gradient.py
+++ common/gradient.py
@@ -1,52 +1,80 @@
 # coding: utf-8
 import numpy as np
 
-def _numerical_gradient_1d(f, x):
-    h = 1e-4 # 0.0001
+def numerical_gradient_1d(f: callable, x: np.ndarray) -> np.ndarray:
+    """Compute the numerical gradient of a 1-dimensional function.
+
+    Args:
+        f (callable): The function for which the gradient is calculated.
+        x (np.ndarray): The input array.
+
+    Returns:
+        np.ndarray: The numerical gradient of the function at the given input.
+    """
+    h = 1e-4 # Step size for numerical differentiation
     grad = np.zeros_like(x)
     
-    for idx in range(x.size):
-        tmp_val = x[idx]
-        x[idx] = float(tmp_val) + h
-        fxh1 = f(x) # f(x+h)
-        
-        x[idx] = tmp_val - h 
-        fxh2 = f(x) # f(x-h)
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        
-        x[idx] = tmp_val # 값 복원
-        
+    for idx, tmp_val in enumerate(x):
+        # Calculate f(x + h)
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # Calculate f(x - h)
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        # Numerical gradient computation
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+
+        # Restore the original value of x[idx]
+        x[idx] = tmp_val
+
     return grad
 
 
-def numerical_gradient_2d(f, X):
+def numerical_gradient_2d(f: callable, X: np.ndarray) -> np.ndarray:
+    """Compute the numerical gradient of a 2-dimensional function.
+
+    Args:
+        f (callable): The function for which the gradient is calculated.
+        X (np.ndarray): The input array. If 1-dimensional, computes 1D numerical gradient.
+
+    Returns:
+        np.ndarray: The numerical gradient of the function at the given input.
+    """
     if X.ndim == 1:
-        return _numerical_gradient_1d(f, X)
+        return numerical_gradient_1d(f, X)
     else:
         grad = np.zeros_like(X)
         
         for idx, x in enumerate(X):
-            grad[idx] = _numerical_gradient_1d(f, x)
+            grad[idx] = numerical_gradient_1d(f, x)
         
         return grad
 
 
-def numerical_gradient(f, x):
+def numerical_gradient(f: callable, x: np.ndarray) -> np.ndarray:
+    """Compute the numerical gradient of a function.
+
+    Args:
+        f (callable): The function for which the gradient is calculated.
+        x (np.ndarray): The input array.
+
+    Returns:
+        np.ndarray: The numerical gradient of the function at the given input.
+    """
     h = 1e-4 # 0.0001
     grad = np.zeros_like(x)
     
-    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
-    while not it.finished:
-        idx = it.multi_index
-        tmp_val = x[idx]
-        x[idx] = float(tmp_val) + h
-        fxh1 = f(x) # f(x+h)
-        
-        x[idx] = tmp_val - h 
-        fxh2 = f(x) # f(x-h)
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        
-        x[idx] = tmp_val # 값 복원
-        it.iternext()   
-        
+    for idx, tmp_val in np.ndenumerate(x):
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+
+        x[idx] = tmp_val
+
     return grad
